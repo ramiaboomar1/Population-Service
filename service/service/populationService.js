e317a8b0b60fa72db41affc7dfd8acf71ae54f5e
Rami <aboomar.2@osu.edu>
2023-07-18 16:58:20 -0400

    refactored to allow for search for cities with spaces

--- service/service/populationService.js
+++ service/service/populationService.js
@@ -1,7 +1,10 @@
 const dbService = require("./dbService");
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string
+    .split(" ")
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(" ");
 }
 
 const fetchPopulation = async (request, reply) => {
@@ -9,14 +12,24 @@ const fetchPopulation = async (request, reply) => {
   const city = capitalizeFirstLetter(request.params.city.toLowerCase());
   const dbResponse = dbService.fetchDataList();
 
-  if (!dbResponse[state] || !dbResponse[state][city]) {
+  if (!dbResponse[state]) {
     return reply
       .status(400)
-      .send({ error: "Bad Request", message: "City not found" });
+      .send({ error: "Bad Request", message: "State not found" });
   } else {
     try {
-      const population = dbResponse[state][city];
-      return reply.status(200).send({ population });
+      let dbCity = Object.keys(dbResponse[state]).filter(
+        (currentCity) =>
+          capitalizeFirstLetter(currentCity.toLowerCase()) === city
+      );
+      if (dbCity.length > 0) {
+        const population = dbResponse[state][dbCity[0]];
+        return reply.status(200).send({ population });
+      } else {
+        return reply
+          .status(400)
+          .send({ error: "Bad Request", message: "City not found" });
+      }
     } catch (error) {
       reply.status(500).send(error);
     }
@@ -28,24 +41,25 @@ const updatePopulation = async (request, reply) => {
   const city = capitalizeFirstLetter(request.params.city.toLowerCase());
   const population = request.body;
   const dbResponse = dbService.fetchDataList();
-
+  console.log(state, city);
   if (!population || isNaN(population)) {
     return reply.status(400).send({ error: "Invalid population data" });
   }
   try {
     if (!dbResponse[state]) {
       dbResponse[state] = {};
-    }
-
-    if (dbResponse[state][city]) {
       dbResponse[state][city] = parseInt(population);
+      await dbService.updateDatalist(dbResponse);
+      return reply.status(201).send({ message: "Data created" });
+    } else {
+      let dbCity = Object.keys(dbResponse[state]).filter(
+        (currentCity) =>
+          capitalizeFirstLetter(currentCity.toLowerCase()) === city
+      );
+      dbResponse[state][dbCity[0]] = parseInt(population);
       dbService.updateDatalist(dbResponse);
       return reply.status(200).send({ message: "Data update" });
     }
-
-    dbResponse[state][city] = parseInt(population);
-    await dbService.updateDatalist(dbResponse);
-    return reply.status(201).send({ message: "Data created" });
   } catch (error) {
     reply.status(500).send(error);
   }
